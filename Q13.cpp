84f7321de3a385db362cd4ba65d234fa8bf30577
Navneet Kumar <76484161+NavneetKumar2907@users.noreply.github.com>
2022-11-06 06:16:40 -0800

    Create Q13.cpp

---
+++ Q13.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int last = 1;
+        for(int i = 1 ; i <= n; i++){
+            int ch = i;
+            while(ch-- > 0){
+                cout << last++ <<" ";
+            }cout<<"\n";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
